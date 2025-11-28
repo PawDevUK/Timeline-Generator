eb0204139ab44b323c665e4abc8aba73c1214b61
PawDevUK <pfsiwek@gmail.com>
2025-11-28 18:19:31 +0000

    Refactor layout of repository list for improved alignment and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
 		<div className='py-8 lg:pr-8 flex-1 w-full lg:border-r border-gray-300 self-stretch'>
 			<div className='flex flex-col gap-6'>
 				<h3 className='text-2xl font-semibold'>List of Repositories.</h3>
-				<div className='flex-1'>
-					<div className='bg-white rounded-lg shadow p-4'>
+				<div className='flex justify-center pt-12'>
+					<div className='bg-white rounded-lg shadow p-4 w-[50vw]'>
 						<div className='flex flex-col gap-4'>
 							{repos.map((repo) => (
 								<div key={repo.name} className='flex items-center justify-between border-b border-gray-200 pb-4 mb-4 last:border-b-0 last:mb-0'>
