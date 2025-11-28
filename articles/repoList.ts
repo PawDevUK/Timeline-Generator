3146feb601bb5ccd578434c8c7f665f7910c9f3c
PawDevUK <pfsiwek@gmail.com>
2025-11-28 18:10:30 +0000

    Add repository list with metadata for improved display and functionality

---
+++ articles/repoList.ts
@@ -0,0 +1,22 @@
+const repos = [
+	{
+		name: 'filesconverto',
+		date: 'Nov 25',
+		iconUrl: 'https://api-frameworks.vercel.sh/framework-logos/next.svg',
+		importUrl: '/new/import?=filesconverto',
+	},
+	{
+		name: 'Portfolio-react',
+		date: 'Nov 25',
+		iconUrl: 'https://api-frameworks.vercel.sh/framework-logos/react.svg',
+		importUrl: '/new/import?=Portfolio-react',
+	},
+	{
+		name: 'Portfolio-Server',
+		date: 'Nov 14',
+		iconUrl: 'https://api-frameworks.vercel.sh/framework-logos/express.svg',
+		importUrl: '/new/importe=Portfolio-Server',
+	},
+];
+
+export default repos;
