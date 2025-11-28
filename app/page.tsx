e8867d047913fc956bdf5828801c90e13a0d77ec
PawDevUK <pfsiwek@gmail.com>
2025-11-28 18:27:34 +0000

    Refactor Home component layout for improved structure and alignment

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import repos from '../articles/repoList';
 
-// RepoList.tsx
-import React from 'react';
-
 export default function Home() {
 	return (
 		<div className='py-8 lg:pr-8 flex-1 w-full lg:border-r border-gray-300 self-stretch'>
-			<div className='flex flex-col gap-6'>
-				<h3 className='text-2xl font-semibold'>List of Repositories.</h3>
-				<div className='flex justify-center pt-12'>
-					<div className='bg-white rounded-lg shadow p-4 w-[50vw]'>
+			<div className='flex flex-col  gap-6'>
+				<div className='w-full text-center md:text-left md:pl-[100px]'>
+					<h3 className='text-2xl font-semibold'>List of Repositories.</h3>
+				</div>
+				<div className='flex justify-center pt-2'>
+					<div className='bg-white rounded-lg shadow p-4 sm:w-[50vw]'>
 						<div className='flex flex-col gap-4'>
 							{repos.map((repo) => (
 								<div key={repo.name} className='flex items-center justify-between border-b border-gray-200 pb-4 mb-4 last:border-b-0 last:mb-0'>
