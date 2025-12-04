506248264432ce97cecd33c28abaf135b052d295
pawel <pfsiwek@gmail.com>
2025-12-04 11:05:12 +0000

    Add user schema definition with Mongoose for user management

---
+++ app/api/db/schema/user.schema.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+
+export interface IUser extends Document {
+	username: string;
+	password: string;
+	email?: string;
+	createdAt: Date;
+}
+
+export const UserSchema: Schema<IUser> = new Schema({
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	email: { type: String, required: false, unique: true },
+	createdAt: { type: Date, default: Date.now },
+});
