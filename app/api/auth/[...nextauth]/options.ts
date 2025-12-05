ba4554cf714fedcc72d9736c79024740fbabe47b
PawDevUK <pfsiwek@gmail.com>
2025-12-05 11:40:45 +0000

    Add NextAuth configuration with GitHub and Credentials providers

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,22 @@
+import type { NextAuthOptions } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+import { CredentialsProvider } from 'next-auth/providers/credentials';
+
+export const options: NextAuthOptions = {
+	providers: [
+		GitHubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+		}),
+		CredentialsProvider({
+			name: 'Credentials',
+			credentials: {
+				username: {
+					lable: 'Username:',
+					type: 'text',
+					placeholder: 'your-username',
+				},
+			},
+		}),
+	],
+};
