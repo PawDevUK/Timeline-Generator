55e1561ea8de7d2993af81a3ab110a73e0377ee4
PawDevUK <pfsiwek@gmail.com>
2025-12-06 12:16:05 +0000

    Add user registration route to handle new user creation

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,17 @@
+import { User } from '@/app/api/db/models/user.model';
+import { NextResponse } from 'next/server';
+import { dbConnect } from '@/app/api/db/db';
+
+await dbConnect();
+
+export async function POST(request: Request) {
+	const { username, password, email } = await request.json();
+
+	const existingUser = await User.findOne({ username });
+	if (existingUser) {
+		return NextResponse.json({ error: 'User already exists' }, { status: 409 });
+	}
+	const newUser = new User({ username, password, email });
+	await newUser.save();
+	return NextResponse.json({ message: 'User created', user: { username, email } }, { status: 201 });
+}
