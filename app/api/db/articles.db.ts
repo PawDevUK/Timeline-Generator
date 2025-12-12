391a3c4fbfec0801a3b168f6d2becd2d86796903
PawDevUK <pfsiwek@gmail.com>
2025-12-12 09:54:43 +0000

    Add CRUD functions for article management in articles.db.ts

---
+++ app/api/db/articles.db.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { ArticleType } from '../types/article.type';
+
+//  Add article
+export async function AddArticle(Article: typeof mongoose.Model, article: ArticleType) {
+	try {
+	} catch (error) {}
+}
+//  Get all articles
+export async function GetAllArticles(Article: typeof mongoose.Model) {
+	try {
+	} catch (error) {}
+}
+//  Get Article by id
+export async function GetArticle(Article: typeof mongoose.Model, articleId: ArticleType) {
+	try {
+	} catch (error) {}
+}
+//  Edit article by id
+export async function EditArticle(Article: typeof mongoose.Model, articleId: ArticleType) {
+	try {
+	} catch (error) {}
+}
+// Delete article by id.
+export async function DeleteArticle(Article: typeof mongoose.Model, articleId: ArticleType) {
+	try {
+	} catch (error) {}
+}
