fe8fd1dda8f4bf64fd4eb5dd02ec5f994822c350
PawDevUK <pfsiwek@gmail.com>
2025-12-12 14:47:17 +0000

    Implement session management and loading state in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+'use client';
 import { redirect } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
-	redirect('/dashboard');
-	return null;
+	const { data: session, status } = useSession();
+
+	// try {
+	// 	console.log('Session', session, 'Status', status);
+	// } catch (err) {
+	// 	console.log(err);
+	// }
+
+	if (status === 'loading') {
+		return <div>Loading...</div>;
+	}
+
+	if (!session) {
+		console.log('Session', session);
+		redirect('/login');
+	}
+
+	return <div>Dashboard</div>;
 }
