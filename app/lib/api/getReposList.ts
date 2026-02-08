6d543a0d489df78f24e5868b7ed4c5c84ec52c70
pawel <pfsiwek@gmail.com>
2026-02-08 20:50:24 +0000

    Remove fetchRepoList function as it is no longer needed

--- app/lib/api/getReposList.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchRepoList(user: string) {
-	const res = await fetch(`/api/getUserRepoList?user=${user}`);
-	if (!res.ok) throw new Error('Failed to fetch repo list');
-	return res.json();
-}
