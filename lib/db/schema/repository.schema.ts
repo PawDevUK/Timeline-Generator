3baa2c46d72d2a550006dd70f1bde539e5419354
pawel <pfsiwek@gmail.com>
2026-02-09 23:12:08 +0000

    Refactor RepositoryDocument interface to extend RepoList and update schema fields

--- lib/db/schema/repository.schema.ts
+++ lib/db/schema/repository.schema.ts
@@ -1,12 +1,29 @@
 import { Schema, Document } from 'mongoose';
 import { ArticleSchema } from './article.schema';
-import { RepositoryType } from '../../types/repository.type';
+import { RepoList } from '@/types/repoList.types';
 
-export interface RepositoryDocument extends RepositoryType, Document {}
+export interface RepositoryDocument extends RepoList, Document {}
 
 export const RepositorySchema: Schema<RepositoryDocument> = new Schema({
+	id: { type: Number },
+	node_id: { type: String },
 	name: { type: String, required: true },
-	user: { type: String, required: true },
-	articles: [ArticleSchema],
-	createdAt: { type: Date, default: Date.now },
+	full_name: { type: String },
+	private: { type: Boolean, default: false },
+	owner: {
+		login: { type: String, required: true },
+		id: { type: Number },
+	},
+	html_url: { type: String },
+	url: { type: String },
+	created_at: { type: String },
+	updated_at: { type: String },
+	pushed_at: { type: String },
+	date: { type: String },
+	language: { type: String },
+	TLG: {
+		tracking: { type: Boolean, default: false },
+		daysActiveCommitts: [{ type: String }],
+		articles: [ArticleSchema],
+	},
 });
