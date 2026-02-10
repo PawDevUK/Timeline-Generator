42e3ee7cc3d17f636ef4c40e6632dec82ce2b57e
PawDevUK <pfsiwek@gmail.com>
2026-02-10 14:36:37 +0000

    Add RepoStatusPanel component for displaying repository status

---
+++ app/search/RepoStatusPanel.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function RepoStatusPanel() {
+	return (
+		<div className='bg-white rounded-lg shadow-md p-6 flex-1 w-1/2'>
+			<div>RepoStatusPanel</div>
+		</div>
+	);
+}
