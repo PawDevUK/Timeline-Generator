6662c0db295535474e0901d8642ed83e84ca15fd
pawel <pfsiwek@gmail.com>
2026-02-12 11:48:30 +0000

    fix: remove unused sidebar links for improved navigation clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,28 +60,6 @@ function SidebarContent() {
 							Time Line
 						</a>
 					</li>
-
-					<li>
-						<a href='#' className='flex items-center px-3 py-2 rounded-lg text-gray-700 hover:bg-gray-100 hover:text-indigo-600'>
-							<svg className='h-5 w-5 mr-2 text-indigo-600' fill='none' stroke='currentColor' strokeWidth='1.5' viewBox='0 0 24 24'>
-								<path
-									d='M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75'
-									strokeLinecap='round'
-									strokeLinejoin='round'
-								/>
-							</svg>
-							Documents
-						</a>
-					</li>
-					<li>
-						<a href='#' className='flex items-center px-3 py-2 rounded-lg text-gray-700 hover:bg-gray-100 hover:text-indigo-600'>
-							<svg className='h-5 w-5 mr-2 text-indigo-600' fill='none' stroke='currentColor' strokeWidth='1.5' viewBox='0 0 24 24'>
-								<path d='M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z' strokeLinecap='round' strokeLinejoin='round' />
-								<path d='M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z' strokeLinecap='round' strokeLinejoin='round' />
-							</svg>
-							Reports
-						</a>
-					</li>
 				</ul>
 			</nav>
 			<div className='mt-auto flex items-center gap-2 pt-8'>
