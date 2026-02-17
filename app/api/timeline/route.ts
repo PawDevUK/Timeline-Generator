4032905cd344ef98825edce3ab1f39564a12733e
pawel <pfsiwek@gmail.com>
2026-02-17 10:30:10 +0000

    feat: update GET function to accept request parameter and improve timeline sync logic

--- app/api/timeline/route.ts
+++ app/api/timeline/route.ts
@@ -1,10 +1,20 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { GetAllRepositories } from '@/lib/db/repository.db';
 import { dbConnect } from '@/lib/db/db';
 import { combineTimeLine } from '@/app/utils/combineTimeLine';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
 	await dbConnect();
+	// Time Line sync with commits pushed to git hub. Every time app loads time line or portfolio loads time line,
+	// it is updated.
+	// I need to change update till day early as sync can be triggered in the morning and creates the article but later on this
+	// day there is more commits but they are not saved as app sees that there is article already.
+	// Sinc should be run with one day delley. If sync is run now ( 10:00am ) it should sync repos till yeasterday 23:59.
+	const baseUrl = new URL(request.url).origin;
+	fetch(`${baseUrl}/api/repositories/sync`, {
+		method: 'POST',
+	});
+
 	try {
 		const result = await GetAllRepositories();
 		if (!result.success) {
