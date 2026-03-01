4e016064ed0224cd1a30c21797818f06dfcc6af8
PawDevUK <p.f.siwek@gmail.com>
2026-03-01 19:45:11 +0000

    feat: extend NextAuth module to include user session properties

--- grlobal.d.ts
+++ grlobal.d.ts
@@ -1,1 +1,15 @@
 declare module '*.css';
+
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+	interface Session {
+		user: {
+			id: string;
+			name?: string | null;
+			email?: string | null;
+			image?: string | null;
+			isOwner?: boolean;
+		};
+	}
+}
