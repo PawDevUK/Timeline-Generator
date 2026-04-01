e34826d46804f5423b36775a37ad5066af2f82e7
pawel <pfsiwek@gmail.com>
2026-04-01 19:24:59 +0100

    fix: improve article validation process in POST endpoint and handle updates more efficiently

--- app/api/repositories/updateBadArt/route.ts
+++ app/api/repositories/updateBadArt/route.ts
@@ -3,32 +3,40 @@ import { GetAllRepositories } from '@/lib/db/repository.db';
 import { dbConnect } from '@/lib/db/db';
 import { callOpenAI } from '@/lib/chatGPT/callOpenAI/callOpenAI';
 import { systemPrompt, getUserPrompt } from './prompts';
-
+import { Types } from 'mongoose';
+const baseUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
+import { Repository } from '@/lib/db/models/repository.model';
 export async function POST(request: NextRequest) {
 	// Function needs to fetch article and send it to OpenAI for validation.
 	// With set of rules/prompts it needs check if article is structuree in correct way.
 	await dbConnect();
 	try {
-		const openAIresponse = await callOpenAI(getUserPrompt, systemPrompt, article);
-		console.log(openAIresponse);
 		const allRepos = await GetAllRepositories();
-		const responsArr = [];
 		if (allRepos.success && allRepos.data) {
 			console.log('Fetched Repositories');
 			for (const repo of allRepos.data) {
-				for (const article of repo.TLG.articles) {
+				for (const article of repo.TLG.articles as { _id?: Types.ObjectId; description: string }[]) {
 					const openAIresponse = await callOpenAI(getUserPrompt, systemPrompt, article.description);
-					console.log(openAIresponse);
-					responsArr.push(openAIresponse);
+					const resJson = JSON.parse(openAIresponse);
+
+					if (resJson.articleNeedsUpdate) {
+						console.log('Updating article:', resJson);
+						await fetch(`${baseUrl}/api/repositories/articles/${article._id}`, {
+							method: 'PUT',
+							headers: { 'Content-Type': 'application/json' },
+							body: JSON.stringify({
+								description: resJson.updatedText,
+							}),
+						}).then((res) => console.log(res));
+					}
 				}
 			}
 		}
-		// if (!result.success) {
-		// 	return NextResponse.json({ error: result.error }, { status: 500 });
-		// }
-		// const timeline = combineTimeLine(result.data || []);
+		if (!allRepos.success) {
+			return NextResponse.json({ error: allRepos.error }, { status: 500 });
+		}
 
-		return NextResponse.json(openAIresponse, { status: 200 });
+		return NextResponse.json('All repositories are updated.', { status: 200 });
 	} catch (error) {
 		console.error('Error in GET /api/timeline:', error);
 		return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
